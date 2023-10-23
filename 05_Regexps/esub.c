0431ad95ef3e9fbe83dd426125951ca8895c30b6
Egor-eth <egor.prih@gmail.com>
2023-10-24 00:02:12 +0300

    05_Regexps fixes

--- 05_Regexps/esub.c
+++ 05_Regexps/esub.c
@@ -27,7 +27,6 @@ void compile_sub_regex()
     int error_code;
     if((error_code = regcomp(&sub_regex_comp, SUB_REGEX_STR, 0))) {
         print_error(error_code, &sub_regex_comp);
-        regfree(&sub_regex_comp);
         exit(1);
     }
     _sub_regex_compiled = true;
@@ -61,11 +60,13 @@ unsigned sub_find_next(const char *substitution, regmatch_t *pmatch)
 {
     const char *ptr = substitution;
 
-    if(regexec(&sub_regex_comp, ptr, 1, pmatch, 0)) {
+    if(!regexec(&sub_regex_comp, ptr, 1, pmatch, 0)) {
         const char *ptr = substitution + pmatch->rm_so + 1;
         long idx;
-        char *tmp = malloc(pmatch->rm_so - pmatch->rm_eo + 1);
-        strncpy(tmp, ptr, pmatch->rm_so - pmatch->rm_eo);
+        size_t alloc_size = pmatch->rm_eo - pmatch->rm_so;
+        char *tmp = malloc(alloc_size);
+        strncpy(tmp, ptr, alloc_size - 1);
+        //printf("%s\n", tmp);
         idx = atol(tmp);
         free(tmp);
         if(idx < 0) {
@@ -76,22 +77,22 @@ unsigned sub_find_next(const char *substitution, regmatch_t *pmatch)
     return 0;
 }
 
-size_t substitute(char *dst, size_t dst_size, regoff_t start, regoff_t end, const char *src, size_t n) {
+size_t substitute(char **dst, size_t dst_size, regoff_t start, regoff_t end, const char *src, size_t n) {
     const size_t diff = n - (end - start);
 
     if(diff > 0) {
-        dst = realloc(dst, dst_size += ((diff / REALLOC_STEP) + 1) * REALLOC_STEP);
+        *dst = realloc(*dst, dst_size += ((diff / REALLOC_STEP) + 1) * REALLOC_STEP);
     }
 
-    char *postfix = dst + end;
-    memmove(postfix + diff, postfix, strlen(dst) - end + 2); //null and start
+    char *postfix = *dst + end;
+    memmove(postfix + diff, postfix, strlen(*dst) - end + 2); //null and start
 
-    memcpy(dst + start, src, n);
+    memcpy(*dst + start, src, n);
 
     return dst_size;
 }
 
-size_t substitute_string(char *dst, size_t dst_size, const char *src, const regmatch_t *match, unsigned sub_index)
+size_t substitute_string(char **dst, size_t dst_size, const char *src, const regmatch_t *match, unsigned sub_index)
 {
     const regoff_t src_start = match->rm_so;
     const regoff_t src_end = match->rm_eo;
@@ -100,9 +101,9 @@ size_t substitute_string(char *dst, size_t dst_size, const char *src, const regm
 
     regmatch_t sub_pmatch;
     unsigned s;
-    if((s = sub_find_next(dst, &sub_pmatch)) != sub_index) {
+    if((s = sub_find_next(*dst, &sub_pmatch)) != sub_index) {
         fprintf(stderr, "Unknown substitution: %u\n", s);
-        free(dst);
+        free(*dst);
         return 0;
     };
 
@@ -143,14 +144,14 @@ int main(int argc, char **argv)
             strcpy(buf, substitution);
 
             for(unsigned i = 1; i < pmatch_size; ++i) {
-                if(!(buf_size = substitute_string(buf, buf_size, str, pmatch + i, i))) {
+                if((buf_size = substitute_string(&buf, buf_size, str, pmatch + i, i)) == 0) {
                     regfree(&regex);
                     free(pmatch);
                     free(dst);
                     return 1;
                 }
             }
-            substitute(dst, dst_size, pmatch[0].rm_so, pmatch[1].rm_eo, buf, strlen(buf));
+            substitute(&dst, dst_size, pmatch[0].rm_so, pmatch[1].rm_eo, buf, strlen(buf));
             free(buf);
         }
 
